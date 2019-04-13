93c4f15197602369415370fc225e6954cb0ff436
thjubeck <thjubeck@users.noreply.github.com>
2019-04-13 03:28:11 -0400

    Add files via upload

---
+++ PushButton/PushButton.cpp
@@ -0,0 +1,160 @@
+#include "PushButton.h"
+
+PushButton::PushButton(int input,int output,int eepromaddr,bool bUseEEPROM,long debounceDelay,int maxeeprom )
+{
+  _input = input;
+  _output = output;
+  _eepromaddr = eepromaddr;
+  _bUseEEPROM = bUseEEPROM;
+  _debounceDelay = debounceDelay;
+  EEPROM.begin(maxeeprom);
+}
+
+void PushButton::begin(int InputType=0, int OutputType=0)
+{
+   if (InputType == 0)
+  {
+     _LowValIn = LOW;
+     _HighValIn = HIGH;
+     pinMode(_input,INPUT);
+  }
+  else if (InputType == 1)
+  {
+     _LowValIn = HIGH;
+     _HighValIn = LOW;
+     pinMode(_input,INPUT);
+  }
+   else if (InputType == 2)
+  {
+     _LowValIn = LOW;
+     _HighValIn = HIGH;
+     pinMode(_input,INPUT_PULLUP);
+  }
+
+
+  if (OutputType == 0)
+  {
+     _LowValOut = LOW;
+     _HighValOut = HIGH;
+     pinMode(_output,OUTPUT);
+  }
+  else if (OutputType == 0)
+  {
+     _LowValOut = HIGH;
+     _HighValOut = LOW;
+     pinMode(_output,OUTPUT);
+  }
+
+
+if (OutputType == 0)
+  {
+     _LowValOut = LOW;
+     _HighValOut = HIGH;
+  }
+  else if (OutputType == 1)
+  {
+     _LowValOut = HIGH;
+     _HighValOut = LOW;
+  }
+
+}
+
+
+void PushButton::read(long _now){
+
+//State Machine Control
+  _CurrentReading = digitalRead(_input);
+  _PreviousState = _CurrentState;
+  //State Machine Section
+  switch (_CurrentState) {
+    case 0: //Home
+      //Home State for the State MAchine
+      _CurrentState = 1;
+    break;
+
+    case 1: //Wait for switch
+      //Wait for the switch to go low
+      if (_CurrentReading == _LowValIn) {_CurrentState = 2;}
+    break;
+
+    case 2: //Switch Pushed
+      //Record the time and proceed to ARMED
+      _lastDebounceTime = _now;
+      _CurrentState = 3;
+    break;
+
+    case 3: //Switch high
+      //Check to see if the proper delay has passed.  If a bounce occures then RESET
+      if (_now - _lastDebounceTime > _debounceDelay) {_CurrentState = 4;}
+      if (_CurrentReading == _HighValIn) {_CurrentState = 0;}
+    break;
+
+    case 4: //Switch is triggered now wait for off
+      //If switch goes high, then TRIGGER.
+      if (_CurrentReading == _HighValIn) {_CurrentState = 5;}
+    break;
+
+    case 5: //Switch is off, Go to home state
+      //reset the State Machine
+      _CurrentState = 0;
+    break;
+
+  }
+}
+
+
+void PushButton::check()
+{
+
+  //Run function to toggle if needed
+  if(_PreviousState != _CurrentState) //if the state has changed and the current step is 4 then toggle lights
+      if (_CurrentState == 4){               //this prevents multiple toggles as the state is 4 for as long as the switch is held
+           toggle();
+       }
+}
+
+
+
+//Toggle function
+void PushButton::toggle()
+ {
+  //Check if this is the first run
+  if (_Start == true){
+  //If it is not first run then toggle value
+      _Val ^= 1; // xor current value with 1 (causes value to toggle)
+      digitalWrite(_output, _Val);
+    //store the value to EEPROM address 0 and commit changes to EEPROM
+
+    if (_bUseEEPROM) {
+      EEPROM.write(_eepromaddr,_Val);
+      EEPROM.commit();
+    }
+  }
+  //If this is the first run then set all values accordingly
+  else {
+  //Set relay variable from the EEPROM
+      if (_bUseEEPROM) {
+      _Val = EEPROM.read(_eepromaddr);
+      digitalWrite(_output,_Val);
+      }
+      else
+      {
+        digitalWrite(_output,_LowValOut);
+      }
+   //Set the start bit to true. This function is only ran once at startup
+      _Start = true;
+  }
+ 
+ }
+
+
+
+
+void PushButton::handle()
+{
+  _now = millis(); 
+  read(_now);
+  check();
+
+}
+  
---
+++ PushButton/PushButton.h
@@ -0,0 +1,44 @@
+#ifndef PushButton_h
+#define PushButton_h
+
+#include <EEPROM.h>
+#include "Arduino.h"
+
+class PushButton
+{
+  
+public:
+  PushButton(int input,int output,int eepromaddr=0,bool bUseEeprom=false,long debounceDelay=100,int maxeeprom=512);
+  void begin(int,int);
+  void read(long);
+  void toggle();
+  void check();
+  void handle();
+private:
+  int _input;
+  int _output;
+  //Define Outputs, inputs, and if started bit. 
+  boolean _Start = false;
+  int _Val  = 0;  //Define Val as 0
+  //State Machine Variables
+  int _CurrentState = 0;
+  int _PreviousState= 0;
+  int _CurrentReading = 0;           // the current reading from the input pin
+  long _lastDebounceTime = 0;  // the last time the output pin was toggled
+  
+  bool  _bUseEEPROM;
+  
+  bool _LowValIn;
+  bool _HighValIn;
+  bool _LowValOut;
+  bool _HighValOut;
+
+  
+  long _debounceDelay;
+  long _now =0 ;
+  int _eepromaddr;
+  long _printline;
+};
+
+
+#endif
